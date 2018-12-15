53a5385d5b47f7c902897a0a009554564d134444
Esteban <gopovesteban2@gmail.com>
2018-12-15 11:47:56 -0600

    Update Factura.java

--- herokuapi/herokuapi/src/main/java/com/projects/cloud/herokuapi/models/Factura.java
+++ herokuapi/herokuapi/src/main/java/com/projects/cloud/herokuapi/models/Factura.java
@@ -6,16 +6,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection="facturas")
 public class Factura {
 
-	public Factura(String status, String idPaquete) {
+	public Factura(String id,String idPaquete,String status ) {
 		super();
-		this.status = status;
+		this.id = id;
 		this.idPaquete = idPaquete;
+		this.status = status;
 	}
 
 	@Id 
-	private String status;
+	private String id;
 	
 	private String idPaquete;
+	
+	private String status;
+	
+	
+	
 
 	public String getStatus() {
 		return status;
@@ -31,6 +37,14 @@ public class Factura {
 
 	public void setIdPaquete(String idPaquete) {
 		this.idPaquete = idPaquete;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 	
 	
